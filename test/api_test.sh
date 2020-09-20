55fec6456f754fb5b4835a6ff730aa487a69a840
Balou9 <emmanuel.owosekun@gmail.com>
2020-09-20 19:32:29 +0200

    chore: observe status code in upsert test

--- test/api_test.sh
+++ test/api_test.sh
@@ -62,7 +62,7 @@ test_profile_read_200() {
 }
 
 test_profile_upsert_204() {
-  printf "test_profile_upsert_200\n"
+  printf "test_profile_upsert_204\n"
   resp_head="$(mktemp)"
   resp_body="$(mktemp)"
 
@@ -75,5 +75,6 @@ test_profile_upsert_204() {
     -D "$resp_head" \
     "$_BASE_URL/profile/$profile_id"
 
+  cat "$resp_head"
   assert_status "$resp_head" 204
 }
