51c11d0a3d4f33f96ee7c579f5e9780676cb1331
Balou9 <emmanuel.owosekun@gmail.com>
2020-09-26 15:32:25 +0200

    impl: try to force 204

--- lambdas/profile/upsert/index.js
+++ lambdas/profile/upsert/index.js
@@ -1,11 +1,13 @@
 module.exports.handler = async function handler(event, context) => {
 
-  return {
+  var response = {
     "statusCode": 204,
     "headers": {
       "content-type": "application/json",
     },
     "body": ''
   }
+
+  return response
 
 }
