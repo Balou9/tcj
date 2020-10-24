a6b422ee47c60f809d6a820b632380003cede321
Balou9 <emmanuel.owosekun@gmail.com>
2020-10-24 18:32:37 +0200

    chore: reuse tt template

--- lambdas/profile/upsert/index.js
+++ lambdas/profile/upsert/index.js
@@ -3,24 +3,37 @@ const { v4: uuidv4 } = require('uuid')
 const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'})
 
 module.exports.handler = (event, context, callback) => {
+  console.log("DEBUG:::", event)
+  if (!event.body || !event.body.length) {
+    callback(null, response(400))
+  } else {
 
-  var params = {
-    TableName: process.env.PROFILE_TABLE_NAME,
-    Item: {
-      "profile_id": uuidv4(),
-      "profile": event.body
+    const params = {
+      TableName: process.env.MATCH_TABLE_NAME,
+      Item: {
+        "profile_id": profileId(),
+        "profile": JSON.parse(event.body)
+      }
+    }
+
+    dynamodb.put(params, function (err, data) {
+      if (err) callback(err)
+      else callback(null, response(204))
+    })
+  }
+
+}
+
+function profileId () {
+  var profile_ID = "mid_" + uuidv4()
+  return profile_ID
+}
+
+function response (status) {
+  return {
+    "statusCode": status,
+    "headers": {
+      "content-type": "application/json"
     }
   }
-
-
-  var response = {
-    "statusCode": 204,
-    "headers": {
-      "content-type": "application/json",
-    },
-    "body": ''
-  }
-
-  callback(null, response)
-
 }
