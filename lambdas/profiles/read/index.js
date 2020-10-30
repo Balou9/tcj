7c7f5bb16b44ef97e7f105e91ec7f358d8dae69b
Balou9 <emmanuel.owosekun@gmail.com>
2020-10-30 22:27:56 +0100

    fix: async ReadProfileHandler

--- lambdas/profiles/read/index.js
+++ lambdas/profiles/read/index.js
@@ -1,14 +1,23 @@
-module.exports.handler = (event, context, callback) => {
-  var payload = {"profileId": "chalee914"}
+const AWS = require('aws-sdk')
+var dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'})
 
-  var response = {
+module.exports.handler = () => {
+  
+  var params = {
+    TableName : process.env.PROFILE_TABLE_NAME,
+    Key: {
+      "profile_id": event.pathParameters.profile_id
+    }
+  }
+
+  await dynamodb.get(params).promise()
+
+  return {
     "statusCode": 200,
     "headers": {
       "content-type": "application/json",
     },
-    "body": JSON.stringify(payload)
+    "body": JSON.stringify(data)
   }
-
-  callback(null, response)
 
 }
