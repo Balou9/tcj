bbf90c4c3c87facf46b4a451e887be57f7589c59
Balou9 <emmanuel.owosekun@gmail.com>
2021-01-01 11:07:17 +0100

    fix: set key in params

--- lambdas/profiles/upsert/index.js
+++ lambdas/profiles/upsert/index.js
@@ -3,19 +3,20 @@ const s3 = new S3({
   apiVersion: "2006-03-01"
 })
 
-module.exports.handler = async function handler (event, context) {
+module.exports.handler = async function (event, context) {
   try {
     if (!event.profile) {
       return { statusCode: 400 }
     }
 
     await s3.putObject({
-      Key: uuidv4(),
-      Bucket: process.env.PROFILE_BUCKET_NAME,
+      Key: 'Bob',
+      Bucket: process.env.BUCKET_NAME,
       Body: event.profile
     }).promise()
 
     return { statusCode: 204 }
-
+  } catch (err) {
+    return { statusCode: 500 }
   }
 }
