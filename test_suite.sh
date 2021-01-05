e490f0f286a90d728c3188278087118374746e82
Balou9 <emmanuel.owosekun@gmail.com>
2021-01-05 11:39:19 +0100

    fix: reference profile as binary file

--- test_suite.sh
+++ test_suite.sh
@@ -5,7 +5,7 @@ test_profiles_upsert_204() {
 
   aws lambda invoke \
     --function-name tcjam-test-upsertprofilehandler \
-    --payload "$profile" \
+    --payload fileb://profile \
     "$resp_body" \
   > /dev/null
 
