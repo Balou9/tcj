28d18d1a064c210fe0babbba26a9de40513f27f7
Balou9 <emmanuel.owosekun@gmail.com>
2021-01-06 21:50:43 +0100

    test: add multiline sign

--- test_suite.sh
+++ test_suite.sh
@@ -6,7 +6,7 @@ test_profiles_upsert_204() {
   aws lambda invoke \
     --function-name tcjam-test-upsertprofilehandler \
     --payload $profile \
-    $resp_body
+    $resp_body \
   > /dev/null
 
   status=$(cat $resp_body | jq .statusCode)
