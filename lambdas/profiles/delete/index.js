73b87b4d4785433ad8805be2fd33099ed67def11
Balou9 <emmanuel.owosekun@gmail.com>
2021-01-09 18:32:17 +0100

    test: test_profiles_delete_204

--- lambdas/profiles/delete/index.js
+++ lambdas/profiles/delete/index.js
@@ -1,25 +1,23 @@
-const { S3 } = require("aws-sdk");
+const { S3 } = require('aws-sdk')
 const s3 = new S3({
-  apiVersion: "2006-03-01"
-});
+  apiVersion: "2006-03-01",
+  params: { Bucket: process.env.BUCKET_NAME }
+})
 
-module.exports.handler = async function handler ({
-  pathParameters
-}) {
-
-  const params = {
-    Bucket: process.env.BUCKET_NAME,
-    Key: {
-      "profileName": pathParameters.profileName
+module.exports.handler = async function (event, context) {
+  try {
+    if (!event.profileName) {
+      return { statusCode: 400 }
     }
+    await s3.delete({
+      Key: event.profileName
+    }).promise()
+
+    return { statusCode: 204 }
+  } catch (err) {
+    if (err.code === 'NoSuchKey') {
+      return { statusCode: 404 }
+    }
+    return { statusCode: 500 }
   }
-
-  await dynamodb.delete(params).promise()
-
-  return { "statusCode": 204 }
-
-}
-
-if (!process.env.PROFILE_TABLE_NAME) {
-  throw new Error("missing required env var PROFILE_TABLE_NAME");
 }
--- test_suite.sh
+++ test_suite.sh
@@ -81,3 +81,26 @@ test_profiles_read_404() {
   status=$(cat $resp_body | jq .statusCode)
   assert_equal $status 404
 }
+
+test_profiles_delete_204() {
+  printf "test_profiles_delete_204\n"
+  resp_body_upsert="$(mktemp)"
+
+  aws lambda invoke \
+    --function-name tcjam-test-upsertprofilehandler \
+    --payload '{"profileName":"Alice"}' \
+    $resp_body_upsert \
+  > /dev/null
+
+  upsert_status=$(cat $resp_body_upsert | jq .statusCode)
+  assert_equal $status 204
+
+  aws lambda invoke \
+    --function-name tcjam-test-deleteprofilehandler \
+    --payload '{"profileName":"Alice"}' \
+    $resp_body_delete \
+  > /dev/null
+
+  status_delete=$(cat $resp_body_delete | jq .statusCode)
+  assert_equal $status_delete 204
+}
