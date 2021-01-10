a8da12d5be6c7c1587c8baa8078a85ee5e283833
Balou9 <emmanuel.owosekun@gmail.com>
2021-01-10 18:02:45 +0100

    chore: observe err code of deleteprofilehandler

--- lambdas/profiles/delete/index.js
+++ lambdas/profiles/delete/index.js
@@ -15,6 +15,7 @@ module.exports.handler = async function (event, context) {
 
     return { statusCode: 204 }
   } catch (err) {
+    console.log("DEBUG:::ErrCode", err, err.code)
     if (err.code === 'NoSuchKey') {
       return { statusCode: 404 }
     }
